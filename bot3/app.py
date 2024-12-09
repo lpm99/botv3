084dfd55a03224c9209f87763384e066f1c6dcf4
lpm99 <leonid.panov1999@gmail.com>
2024-12-09 21:20:16 +0300

    Update app.py

--- bot3/app.py
+++ bot3/app.py
@@ -20,7 +20,7 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(
     token=TELEGRAM_BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
-bot.my_admins_list = [-1002497986286]
+bot.my_admins_list = []
 
 dp = Dispatcher()
 
